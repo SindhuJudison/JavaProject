0458c63e379fa14d2f8359936cff06bf79a012bf
SindhuJudison <samsv0511@gmail.com>
2022-02-20 11:57:16 +0530

    Git0220

---
+++ src/org/task/GitTest.java
@@ -0,0 +1,24 @@
+package org.task;
+
+public class GitTest {
+
+	public void test1() {
+		
+		System.out.println("test1");
+		
+	}	
+		public void test2() {
+			
+			System.out.println("test2");
+
+		}
+public static void main(String[] args) {
+	
+	GitTest git = new GitTest();
+	git.test1();
+	git.test2();
+			
+}
+}
+
+
